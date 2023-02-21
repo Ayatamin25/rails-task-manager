a8ebd916d2f40b9c0d35488e00a61c040ccf19e5
Ayat Amin <ayat.jafar.amin@gmail.com>
2023-02-21 12:25:57 -0500

    creating a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,20 @@
 class TasksController < ApplicationController
-
   def index
-    @tasks = Task.all
+    @task = Task.all
   end
 
   def show
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task.id)
   end
 
   private
@@ -13,5 +22,4 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
-
 end
